1e8906507a7b2ba9df11855e08724323bf5dd247
cenfun <cenfun@gmail.com>
2024-03-14 12:19:57 +0800

    docs: ts

--- README.md
+++ README.md
@@ -22,19 +22,11 @@ cross-env NODE_OPTIONS="--loader ts-node/esm --no-warnings" npx mcr mocha ./test
 ## Resolve the source for `.ts`
 When collecting coverage data for the `.ts` file, we can also get the sourcemap from [`source-map-cache`](https://nodejs.org/docs/latest/api/cli.html#source-map-cache), but we can't get the runtime source code, instead, what we get is `lineLengths`, which can only generate a fake source, but it cannot be parsed into an AST. Therefore, we need to compile the source for these ts files with `ts-node`.
 ```js
-import * as fs from "fs";
-import * as path from "path";
-import { fileURLToPath } from "url";
-import * as TsNode from 'ts-node';
 const coverageOptions = {
     onEntry: async (entry) => {
-        const filePath = fileURLToPath(entry.url)
-        const originalSource = fs.readFileSync(filePath).toString("utf-8");
-        const fileName = path.basename(filePath);
-        const tn = TsNode.create({});
-        const source = tn.compile(originalSource, fileName);
-        entry.fake = false;
-        entry.source = source;
+        if (entry.fake) {
+            compileEntrySource(entry);
+        }
     }
 }
 ```
--- mcr.config.ts
+++ mcr.config.ts
@@ -1,48 +1,52 @@
-import type { CoverageReportOptions } from "monocart-coverage-reports";
-import * as fs from "fs";
-import * as path from "path";
-import { fileURLToPath } from "url";
+// import type { CoverageReportOptions } from "monocart-coverage-reports";
+import * as fs from 'fs';
+import * as path from 'path';
+import { fileURLToPath } from 'url';
 import * as TsNode from 'ts-node';
 
-const coverageOptions:CoverageReportOptions = {
+const compileEntrySource = (entry) => {
+    // console.log(entry.url);
+    // console.log("fake source", entry.source.length);
+    // transpile fake source
+    const filePath = fileURLToPath(entry.url);
+    // console.log(filePath);
+
+    const originalSource = fs.readFileSync(filePath).toString('utf-8');
+    const fileName = path.basename(filePath);
+
+    const tn = TsNode.create({});
+    const source = tn.compile(originalSource, fileName);
+    // console.log("compiled source", source.length);
+
+    // console.log("======================================")
+    // console.log(entry.source)
+    // console.log("======================================")
+    // console.log(source)
+
+    // console.log("originalSource.length", originalSource.length)
+    entry.source = source;
+    entry.fake = false;
+
+};
+
+const coverageOptions = {
     // logging: "debug",
 
     reports: [
-        ["v8"],
-        ["console-details"]
+        ['v8'],
+        ['console-details']
     ],
 
     entryFilter: {
-        "**/node_modules/**": false,
-        "**/src/**": true
+        '**/node_modules/**': false,
+        '**/src/**': true
     },
 
-    onEntry: async (entry)=> {
-
-        //console.log(entry.url);
-        //console.log("fake source", entry.source.length);
-        // transpile fake source
-        const filePath = fileURLToPath(entry.url)
-        // console.log(filePath);
-
-        const originalSource = fs.readFileSync(filePath).toString("utf-8");
-        const fileName = path.basename(filePath);
-
-        const tn = TsNode.create({});
-        const source = tn.compile(originalSource, fileName);
-        // console.log("compiled source", source.length);
-
-        // console.log("======================================")
-        // console.log(entry.source)
-        // console.log("======================================")
-        // console.log(source)
-
-        // console.log("originalSource.length", originalSource.length)
-        entry.fake = false;
-        entry.source = source;
-
-
+    onEntry: (entry) => {
+        if (entry.fake) {
+            compileEntrySource(entry);
+        }
     }
-}
+};
 
-export default coverageOptions
+export default coverageOptions;
